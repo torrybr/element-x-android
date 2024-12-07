671d477a013eaf984e7f1a1667cffea826f89b38
torrybr <16907963+torrybr@users.noreply.github.com>
2024-12-07 11:35:40 -0500

    working send,stop,start with sdk

--- libraries/matrix/impl/src/main/kotlin/io/element/android/libraries/matrix/impl/room/RustMatrixRoom.kt
+++ libraries/matrix/impl/src/main/kotlin/io/element/android/libraries/matrix/impl/room/RustMatrixRoom.kt
@@ -197,6 +197,7 @@ class RustMatrixRoom(
                                     ),
                                     ts = it.lastLocation.ts,
                                 ),
+                                isLive = it.isLive
                             )
                         }
                     )
@@ -857,6 +858,12 @@ class RustMatrixRoom(
     override suspend fun stopLiveLocationShare(): Result<Unit> = withContext(roomDispatcher) {
         runCatching {
             innerRoom.stopLiveLocationShare()
+        }
+    }
+
+    override suspend fun sendLiveLocation(geoUri: String) = withContext(roomDispatcher) {
+        runCatching {
+            innerRoom.sendLiveLocation(geoUri)
         }
     }
 }
